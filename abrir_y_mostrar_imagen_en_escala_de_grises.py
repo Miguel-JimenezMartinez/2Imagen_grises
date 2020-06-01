e38d12cf019e8e41f7b942c1b1a8f7a761800ad1
MiguelMike54 <66104861+MiguelMike54@users.noreply.github.com>
2020-06-01 17:33:51 -0500

    Escala de grises.
    Se añadio un 0 en la linea 3.

---
+++ abrir_y_mostrar_imagen_en_escala_de_grises.py
@@ -0,0 +1,9 @@
+import cv2                                #Importando OpenCv
+
+imagen=cv2.imread('poeta_halley.jpg',0)   #El codigo será similar al anterior, pero al añadirle un 0 cambiara a escala de grises        
+                                          
+cv2.imshow('Hola soy una imagen en escala de grises', imagen)                                 
+
+cv2.waitKey(0)                            
+
+cv2.destroyAllWindows()                   
