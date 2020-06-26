edd7581bd4e8a01bfd292f52c581a671bd46f901
MiguelMike54 <66104861+MiguelMike54@users.noreply.github.com>
2020-06-26 00:29:07 -0500

    Comente la funcion "cv2.imshow"

--- escala_grises.py
+++ escala_grises.py
@@ -2,7 +2,9 @@ import cv2                                #Importando OpenCv
 
 imagen=cv2.imread('poeta_halley.jpg',0)   #El codigo será similar al anterior, pero al añadirle un 0 cambiara a escala de grises        
                                           
-cv2.imshow('Hola soy una imagen en escala de grises', imagen)                                 
+#cv2.imshow('Hola soy una imagen en escala de grises', imagen)                                 
+
+print("Hola soy una imagen en escala de grises")
 
 cv2.waitKey(0)                            
 
